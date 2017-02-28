48ad5a825cf86bdf748c1b44366250b26c94760d
doliv <d.oliver13@me.com>
2017-02-28 19:06:54 +0100

    update inicialization

--- spring/src/main/java/urteam/event/eventController.java
+++ spring/src/main/java/urteam/event/eventController.java
@@ -1,10 +1,12 @@
 package urteam.event;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class eventController {
 	
 	@Autowired
