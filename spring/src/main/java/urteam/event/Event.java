2d25cdba41fce81b9532fd8d8b41650f5384bffa
doliv <d.oliver13@me.com>
2017-02-28 10:50:03 +0100

    add ecent package

---
+++ spring/src/main/java/urteam/event/Event.java
@@ -0,0 +1,136 @@
+package urteam.event;
+
+import java.sql.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+import urteam.user.*;
+
+@Entity
+public class Event {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	long id;
+
+	private String name;
+	private Long price;
+	private String info;
+	private String main_photo;
+	private String place;
+	private Date start_date;
+	private Date end_date;
+	
+	@OneToOne
+	private Long owner_id;
+	
+	@OneToMany
+	private List<User> participants_IDs;
+
+	public Event() {
+	}
+
+	public Event(String name, Long price, String info, String place, Date start_date, Date end_date) {
+		this.name = name;
+		this.price = price;
+		this.info = info;
+		this.place = place;
+		this.start_date = start_date;
+		this.end_date = end_date;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Long getPrice() {
+		return price;
+	}
+
+	public void setPrice(Long price) {
+		this.price = price;
+	}
+
+	public String getInfo() {
+		return info;
+	}
+
+	public void setInfo(String info) {
+		this.info = info;
+	}
+
+	public String getMain_photo() {
+		return main_photo;
+	}
+
+	public void setMain_photo(String main_photo) {
+		this.main_photo = main_photo;
+	}
+
+	public String getPlace() {
+		return place;
+	}
+
+	public void setPlace(String place) {
+		this.place = place;
+	}
+
+	public Date getStart_date() {
+		return start_date;
+	}
+
+	public void setStart_date(Date start_date) {
+		this.start_date = start_date;
+	}
+
+	public Date getEnd_date() {
+		return end_date;
+	}
+
+	public void setEnd_date(Date end_date) {
+		this.end_date = end_date;
+	}
+
+	public Long getOwner_id() {
+		return owner_id;
+	}
+
+	public void setOwner_id(Long owner_id) {
+		this.owner_id = owner_id;
+	}
+
+	public List<User> getParticipants_IDs() {
+		return participants_IDs;
+	}
+
+	public void setParticipants_IDs(List<User> participants_IDs) {
+		this.participants_IDs = participants_IDs;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ spring/src/main/java/urteam/event/EventRepository.java
@@ -0,0 +1,7 @@
+package urteam.event;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<Event, Long>{
+
+}
