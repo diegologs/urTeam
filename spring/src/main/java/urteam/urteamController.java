2f63cb9db6acd2c6ca88b3f824468f4151a93ff9
doliv <d.oliver13@me.com>
2017-02-28 17:08:23 +0100

    update urteamController

--- spring/src/main/java/urteam/urteamController.java
+++ spring/src/main/java/urteam/urteamController.java
@@ -1,16 +1,49 @@
 package urteam;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import urteam.community.*;
+import urteam.event.*;
+import urteam.user.*;
+
 @Controller
 public class urteamController {
+	
+	@Autowired
+	private UserRepository userRepo;
+	
+	@Autowired
+	private EventRepository eventRepo;
+	
+	@Autowired
+	private CommunityRepository communityRepo;	
+	
+	@PostConstruct
+	public void init (){
+		for (int i = 0; i < 10; i++) {
+			String name = String.valueOf(i);
+			String surname = String.valueOf(i);
+			userRepo.save(new User(name, surname));
+		}
+		
+		for (int i = 0; i < 10; i++) {
+			eventRepo.save(new Event());
+		}
+
+		for (int i = 0; i < 10; i++) {
+			communityRepo.save(new Community());
+		}
+	}
 
 	@RequestMapping("/")
 	public String greeting(Model model) {
 
-		model.addAttribute("name", "World");
+		
 
 		return "index";
 	}
