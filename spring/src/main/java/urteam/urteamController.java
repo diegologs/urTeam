b933bd641a2450639f3a1411aa02f19ff3fbda0e
pablogarciabenayas <pablogarciabenayas@gmail.com>
2017-03-11 00:21:36 +0100

    Fixed uploadimage method

--- spring/src/main/java/urteam/urteamController.java
+++ spring/src/main/java/urteam/urteamController.java
@@ -19,6 +19,7 @@ import org.springframework.ui.Model;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -102,7 +103,7 @@ public class urteamController {
 
 	
 
-	@RequestMapping("/image/upload")
+	@RequestMapping(value = "/image/upload", method = RequestMethod.POST)
 	public String uploadImageFile(Model model, @RequestParam("file") MultipartFile file, String action) {
 
 		String fileName = "test.jpeg";
