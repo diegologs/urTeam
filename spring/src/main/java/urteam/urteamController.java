ddfde1ea93c38b766d20e381b4dc752222956b72
pablogarciabenayas <pablogarciabenayas@gmail.com>
2017-03-11 02:11:42 +0100

    Solved

--- spring/src/main/java/urteam/urteamController.java
+++ spring/src/main/java/urteam/urteamController.java
@@ -4,17 +4,14 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.security.Timestamp;
-import java.util.Calendar;
-import java.util.Date;
+
 import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
