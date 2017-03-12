a4fca30d462cad7386f3c786528b538bb24694cf
pablogarciabenayas <pablogarciabenayas@gmail.com>
2017-03-12 19:01:01 +0100

    Update user entity

--- spring/src/main/java/urteam/user/User.java
+++ spring/src/main/java/urteam/user/User.java
@@ -25,6 +25,8 @@ public class User {
 	private String generatedId = "aleatorio";
 	private String username;
 	private String surname;
+	
+	@Column(unique = true)
 	private String nickname;
 	private String password;
 	private String email;
@@ -204,6 +206,11 @@ public class User {
 
 	public void addCommunity(Community community) {
 		this.communityList.add(community);
+	}
+
+	public void removeCommunity(Community community) {
+		this.communityList.remove(community);
+		
 	}
 
 }
