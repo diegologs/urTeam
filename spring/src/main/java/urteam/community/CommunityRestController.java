ae5fbf28668859fbea237c6b6e16c151ca1cd51b
Frostqui <diego.lopezgarcia@hotmail.com>
2017-03-28 10:45:39 +0200

    Update CommunityRestController

--- spring/src/main/java/urteam/community/CommunityRestController.java
+++ spring/src/main/java/urteam/community/CommunityRestController.java
@@ -16,9 +16,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.multipart.MultipartFile;
 
 import urteam.ConstantsUrTeam;
@@ -36,30 +38,16 @@ import urteam.user.UserRepository;
 @RequestMapping("/api/groups")
 public class CommunityRestController {
 
-	@Autowired
-	private urteamController urteam;
-
+	
 	@Autowired
 	private CommunityService service;
 
-	@Autowired
-	private NewsRepository newsRepo;
-
-	@Autowired
-	private UserRepository userRepo;
-
-	@Autowired
-	private UserComponent userComponent;
-
-	
+		
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public Collection<Community> groups() {
 		
 		return service.findAll();
 	}
-	
-	
-	
 	
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
@@ -72,34 +60,23 @@ public class CommunityRestController {
 		}else{
 			return new ResponseEntity<>(community, HttpStatus.NOT_FOUND);
 		}
-		
-		
+				
 		
 	}
 
 
 	
 
-	@RequestMapping(value = "/group/{id}/edit", method = RequestMethod.PUT)
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Community> addInfo(@PathVariable long id, @RequestParam String info) {
-		// Buscar la comunidad y su creador
-		Community community = service.findOne(id);
-		User ownerCommunity = userRepo.findOne(community.getOwner_id().getId());
-
-		// Comprobar si hay un usuario logueado
-		if ((userComponent.isLoggedUser())) {
-			long userLogged_id = userComponent.getLoggedUser().getId();
-			User userLogged = userRepo.findOne(userLogged_id);
-			
-
-			// Comprobar que el usuario y el dueño del evento son el mismoy si
-			// lo son modificar
-			if (userLogged.getId() == ownerCommunity.getId()) {
-				community.setInfo(info);
-			}
-			service.save(community);
 		
-		}
+		
+		Community community = service.findOne(id);
+		
+		
+		service.editInfo(community, info);
+		
+				
 		
 		if(community != null){
 			return new ResponseEntity<>(community, HttpStatus.OK);
@@ -110,29 +87,14 @@ public class CommunityRestController {
 	}
 
 	
-	@RequestMapping("/group/{id}/addNews")
-	public ResponseEntity<Community> groupEdited(@PathVariable long id, @RequestParam String title, @RequestParam String text) {
-		// Buscar la comunidad y su creador
+	@RequestMapping(value = "/{id}/news", method = RequestMethod.PUT)
+	public ResponseEntity<Community> addNews(@PathVariable long id, @RequestBody News news) {
+		
 		Community community = service.findOne(id);
-		User ownerCommunity = userRepo.findOne(community.getOwner_id().getId());
-
-		// Comprobar si hay un usuario logueado
-		if ((userComponent.isLoggedUser())) {
-			long userLogged_id = userComponent.getLoggedUser().getId();
-			User userLogged = userRepo.findOne(userLogged_id);
-
-			// Comprobar que el usuario y el dueño de la comunidad son el mismo
-			// y si lo son modificar
-			if (userLogged.getId() == ownerCommunity.getId()) {
-				News news = new News(title, text);
-				community.getNews().add(news);
-				newsRepo.save(news);
-			}
-			service.save(community);
 		
-		}
+		service.addNews(community, news);
 		
-			
+				
 		if(community != null){
 			return new ResponseEntity<>(community, HttpStatus.OK);
 		}else{
@@ -141,119 +103,55 @@ public class CommunityRestController {
 			
 	}
 
-/*
-	@RequestMapping("/group/{id}/addImage")
-	public String addImage(Model model, @PathVariable long id, @RequestParam("file") MultipartFile file,
-			HttpServletRequest request) throws ParseException {
-		// Buscar la comunidad y su creador
-		Community community = communityRepo.findOne(id);
-		User ownerCommunity = userRepo.findOne(community.getOwner_id().getId());
 
-		// Comprobar si hay un usuario logueado
-		if ((userComponent.isLoggedUser())) {
-			long userLogged_id = userComponent.getLoggedUser().getId();
-			User userLogged = userRepo.findOne(userLogged_id);
-			model.addAttribute("logged", true);
-			model.addAttribute("user", userLogged);
-			model.addAttribute("communityFollowed", userLogged.getCommunityList().contains(community));
-			// Comprobar si es admin
-			model.addAttribute("admin", request.isUserInRole("ROLE_ADMIN"));
-
-			// Comprobar que el usuario y el dueño de la comunidad son el mismo
-			// y si lo son modificar
-			if (userLogged.getId() == ownerCommunity.getId()) {
-				SimpleDateFormat formater = new SimpleDateFormat("mmddyyyy-hhMMss");
-				Date date = new Date();
-
-				String filename = "imageingallery-" + formater.format(date);
-
-				if (urteam.uploadImageFile(model, file, filename, ConstantsUrTeam.COMMUNITY_IMGS,
-						community.getCommunityId())) {
-					community.addImage(filename);
-				}
-			}
-			communityRepo.save(community);
-			model.addAttribute("community", community);
-			return "redirect:/group/{id}";
-		} else {
-			return "redirect:/group/{id}";
+	@RequestMapping("/{id}/addImage")
+	public ResponseEntity<Community> addImage(@PathVariable long id, @RequestParam("file") MultipartFile file) throws ParseException {
+		
+		
+		Community community = service.findOne(id);
+		
+		service.addImage(community, file);
+		
+		
+		if(community != null){
+			return new ResponseEntity<>(community, HttpStatus.OK);
+		}else{
+			return new ResponseEntity<>(community, HttpStatus.NOT_FOUND);
 		}
 	}
 
-	@RequestMapping("addGroup")
-	public String newEvent() {
-		return "addGroup";
-	}
+	
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Community> add(Community community, @RequestParam("file") MultipartFile file) throws ParseException {
 
-	@RequestMapping("/GroupAdded")
-	public String groupAdded(Model model, Community community, @RequestParam("file") MultipartFile file,
-			HttpServletRequest request) throws ParseException {
-
-		// Filename formater
-		SimpleDateFormat formater = new SimpleDateFormat("mmddyyyy-hhMMss");
-		Date date = new Date();
-
-		// EventId generator
-		SimpleDateFormat communityIdFormater = new SimpleDateFormat("mmddyyyy-hhMMss");
-		String communitytId = communityIdFormater.format(date);
-		community.setCommunityId(communitytId);
-		community.setOwner_id(userComponent.getLoggedUser());
-
-		String filename = "avatar-" + formater.format(date);
-
-		if (urteam.uploadImageFile(model, file, filename, ConstantsUrTeam.COMMUNITY_AVATAR,
-				community.getCommunityId())) {
-			community.setMain_photo(filename);
-		}
-		communityRepo.save(community);
-
-		model.addAttribute("groups_active", true);
-
-		if ((userComponent.isLoggedUser())) {
-			long idUser = userComponent.getLoggedUser().getId();
-			User userLogged = userRepo.findOne(idUser);
-			model.addAttribute("user", userLogged);
-			if (userComponent.getLoggedUser().getId() == userLogged.getId()) {
-				model.addAttribute("logged", true);
-			}
-			model.addAttribute("admin", request.isUserInRole("ROLE_ADMIN"));
-			return "redirect:groups";
-		} else {
-			return "redirect:groups";
+		
+		service.add(community, file);
+		
+		if(community != null){
+			return new ResponseEntity<>(community, HttpStatus.OK);
+		}else{
+			return new ResponseEntity<>(community, HttpStatus.NOT_FOUND);
 		}
 
 	}
 
+	
+	
 	@RequestMapping("/group/{id}/follow")
-	public String follow(Model model, @PathVariable long id, HttpServletRequest request) {
-		// Buscar la comunidad y su creador
-		Community community = communityRepo.findOne(id);
-
-		// Comprobar si hay un usuario logueado
-		if ((userComponent.isLoggedUser())) {
-			long userLogged_id = userComponent.getLoggedUser().getId();
-			User userLogged = userRepo.findOne(userLogged_id);
-			model.addAttribute("logged", true);
-			model.addAttribute("user", userLogged);			
-			// Comprobar si es admin
-			model.addAttribute("admin", request.isUserInRole("ROLE_ADMIN"));
-
-			// Comprobar si la comunidad esta entre las seguidas del usuario
-			if (userLogged.getCommunityList().contains(community)) {
-				userLogged.removeCommunity(community);
-			} else {
-				userLogged.addCommunity(community);
-			}
-			model.addAttribute("communityFollowed", userLogged.getCommunityList().contains(community));
-
-			communityRepo.save(community);
-			userRepo.save(userLogged);
-			model.addAttribute("community", community);
-			return "redirect:/group/{id}";
-		} else {
-			return "redirect:/group/{id}";
+	public ResponseEntity<Community> follow(@PathVariable long id) {
+		
+		Community community = service.findOne(id);
+		
+		service.follow(community);
+		
+		
+		if(community != null){
+			return new ResponseEntity<>(community, HttpStatus.OK);
+		}else{
+			return new ResponseEntity<>(community, HttpStatus.NOT_FOUND);
 		}
 	}
 	
-	*/
-	}
+	
+}
--- spring/src/main/java/urteam/community/CommunityService.java
+++ spring/src/main/java/urteam/community/CommunityService.java
@@ -1,18 +1,37 @@
 package urteam.community;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-/* Este servicio se usará para incluir la funcionalidad que sea 
- * usada desde el BookRestController y el BookWebController
- */
+import urteam.ConstantsUrTeam;
+import urteam.urteamController;
+import urteam.news.News;
+import urteam.user.User;
+import urteam.user.UserComponent;
+import urteam.user.UserRepository;
+
+
 @Service
 public class CommunityService {
 
 	@Autowired
 	private CommunityRepository repository;
+	
+	@Autowired
+	private UserRepository userRepo;
+	
+	@Autowired
+	private UserComponent userComponent;
+	
+	@Autowired
+	private urteamController urteam;
+	
 
 	public Community findOne(long id) {
 		return repository.findOne(id);
@@ -21,12 +40,113 @@ public class CommunityService {
 	public List<Community> findAll() {
 		return repository.findAll();
 	}
-
+	
 	public void save(Community community) {
-		repository.save(community);
+		
+	repository.save(community);
+		
+	}
+
+	public void saveOwner(Community community) {
+		if(isOwner(community)){
+			repository.save(community);
+		}
 	}
 
 	public void delete(long id) {
 		repository.delete(id);
 	}
+
+	public void editInfo(Community community, String info){
+		community.setInfo(info);
+		saveOwner(community);
+	}
+	
+	public void addNews(Community community, News news){
+		community.getNews().add(news);
+		saveOwner(community);
+	}
+	
+	
+	public boolean isOwner(Community community){
+		
+		User ownerCommunity = userRepo.findOne(community.getOwner_id().getId());
+		
+		if ((userComponent.isLoggedUser())) {
+			long userLogged_id = userComponent.getLoggedUser().getId();
+			User userLogged = userRepo.findOne(userLogged_id);
+					
+
+			if (userLogged.getId() == ownerCommunity.getId()) {
+					return true;
+			}else{
+				return false;
+			}
+		
+		}else{
+				return false;
+			
+				
+		}
+	}
+	
+	public void addImage(Community community, MultipartFile file){
+		
+				/*
+				if (urteam.uploadImageFile(model, file, filename, ConstantsUrTeam.COMMUNITY_IMGS,
+						community.getCommunityId())) {
+					community.addImage(filename);
+				}
+				
+				*/
+				saveOwner(community);
+			
+			
+	}
+
+	public void add(Community community, MultipartFile file) {
+		
+		SimpleDateFormat formater = new SimpleDateFormat("mmddyyyy-hhMMss");
+		Date date = new Date();
+
+		// EventId generator
+		SimpleDateFormat communityIdFormater = new SimpleDateFormat("mmddyyyy-hhMMss");
+		String communitytId = communityIdFormater.format(date);
+		community.setCommunityId(communitytId);
+		community.setOwner_id(userComponent.getLoggedUser());
+	
+		String filename = "avatar-" + formater.format(date);
+	
+		/*
+		if (urteam.uploadImageFile(model, file, filename, ConstantsUrTeam.COMMUNITY_AVATAR,
+			community.getCommunityId())) {
+			community.setMain_photo(filename);
+		}
+				
+		*/
+		save(community);
+		
+	}
+
+	public void follow(Community community) {
+		
+		if ((userComponent.isLoggedUser())) {
+		
+			long userLogged_id = userComponent.getLoggedUser().getId();
+			User userLogged = userRepo.findOne(userLogged_id);
+			
+			if (userLogged.getCommunityList().contains(community)) {
+				userLogged.removeCommunity(community);
+			} else {
+				userLogged.addCommunity(community);
+			}
+			
+			save(community);
+			userRepo.save(userLogged);
+		}
+
+		
+		
+	}
+
 }
