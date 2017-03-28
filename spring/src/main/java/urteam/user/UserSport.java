04ac92ce1ba2ffbdf3cc8bafefaa8890e5229d33
pablogarciabenayas <pablogarciabenayas@gmail.com>
2017-03-29 01:28:20 +0200

    Fixed UserSport class

--- spring/src/main/java/urteam/user/UserSport.java
+++ spring/src/main/java/urteam/user/UserSport.java
@@ -14,70 +14,62 @@ import urteam.stats.Stats_dos;
 
 @Entity
 public class UserSport {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
-	
-	private String sportName;
-	public double getSportTotalTime() {
-		return sportTotalTime;
-	}
-
-
-	public void setSportTotalTime(double sportTotalTime) {
-		this.sportTotalTime = sportTotalTime;
-	}
 
 	private double sportTotalTime;
-	
-	@OneToMany(cascade = {CascadeType.ALL})
+	private String sportName;
+
+	@OneToMany(cascade = { CascadeType.ALL })
 	private List<Stats_dos> sportStats = new ArrayList<>();
-	
-	
+
 	public UserSport() {
 	}
-
 
 	public long getId() {
 		return id;
 	}
 
-
 	public void setId(long id) {
 		this.id = id;
 	}
-
 
 	public String getSportName() {
 		return sportName;
 	}
 
-
 	public void setSportName(String sportName) {
 		this.sportName = sportName;
 	}
-
 
 	public List<Stats_dos> getSportStats() {
 		return sportStats;
 	}
 
-
 	public void setSportStats(List<Stats_dos> sportStats) {
 		this.sportStats = sportStats;
 	}
-	
+
 	public void addSportStats(Stats_dos stats) {
 		this.sportStats.add(stats);
 		this.updateSportTotalTime();
 	}
-	
-	public void updateSportTotalTime(){
-		sportTotalTime=0;
+
+	public void updateSportTotalTime() {
+		sportTotalTime = 0;
 		for (Stats_dos times : sportStats) {
-			sportTotalTime =  sportTotalTime + times.getTotalSesionTime();
+			sportTotalTime = sportTotalTime + times.getTotalSesionTime();
 		}
 	}
-	
+
+	public double getSportTotalTime() {
+		return sportTotalTime;
+	}
+
+	public void setSportTotalTime(double sportTotalTime) {
+		this.sportTotalTime = sportTotalTime;
+	}
+
 }
