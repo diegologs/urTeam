2969b48e14e2276d55ca888179c919e028743640
pablogarciabenayas <pablogarciabenayas@gmail.com>
2017-03-31 20:22:18 +0200

    Fixed sport rest service

--- spring/src/main/java/urteam/sport/SportRepository.java
+++ spring/src/main/java/urteam/sport/SportRepository.java
@@ -1,9 +1,9 @@
 package urteam.sport;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SportRepository extends JpaRepository<Sport, Long>{
+public interface SportRepository extends JpaRepository<Sport, Long> {
+	
 	Sport findByName(String name);
+	
 }
