9d3a8c6da64d3fc640c4b359fdbbee610e78c08b
oly13 <d.oliver13@me.com>
2017-04-02 23:57:27 +0200

    delete useless imports

--- spring/src/main/java/urteam/news/News.java
+++ spring/src/main/java/urteam/news/News.java
@@ -4,12 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 
 import com.fasterxml.jackson.annotation.JsonView;
-
-import urteam.community.Community;
-import urteam.community.Community.BasicCommunity;
 
 @Entity
 
