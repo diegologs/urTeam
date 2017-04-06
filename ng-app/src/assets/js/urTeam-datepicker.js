a7a72575b1b1fb9199cb5b415f84953f87797aef
Jose Miguel Garcia <jmgbenayas@gmail.com>
2017-04-06 12:16:07 +0200

    Add images, js and favicon. Navbar component.

---
+++ ng-app/src/assets/js/urTeam-datepicker.js
@@ -0,0 +1,9 @@
+$('.input-daterange').datepicker({
+    format: "dd/mm/yyyy",
+    startDate: '+0d',
+    todayBtn: "linked",
+    language: "es",
+    orientation: "bottom auto",
+    autoclose: true,
+    todayHighlight: true
+});
