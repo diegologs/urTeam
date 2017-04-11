82786052b0844c5e3542fea3896b05a9d2f8fe26
pablogarciabenayas <pablogarciabenayas@gmail.com>
2017-04-11 18:28:29 +0200

    Update events service

--- ng-app/src/app/events/events.service.ts
+++ ng-app/src/app/events/events.service.ts
@@ -3,36 +3,60 @@ import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/Rx';
 
-import {Event} from './events.model';
+import { Event } from './events.model';
 
 
 const BASE_URL = 'https://127.0.0.1:8443/api/events/';
 
 @Injectable()
-export class EventService{
+export class EventService {
 
-    constructor(private http: Http){}
+	constructor(private http: Http) { }
 
-    getEvents() {
+	getEvents() {
 		return this.http.get(BASE_URL)
 			.map(response => response.json())
 			.catch(error => this.handleError(error));
 	}
 
-    getEvent(id: number | string) {
+	getEvent(id: number | string) {
 		return this.http.get(BASE_URL + id)
 			.map(response => response.json())
 			.catch(error => this.handleError(error));
 	}
 
-    getEventMember(id: number | string){
-        return this.http.get(BASE_URL + id +'/members')
+	getEventMember(id: number | string) {
+		return this.http.get(BASE_URL + id + '/members')
+			.map(response => response.json())
+			.catch(error => this.handleError(error));
+	}
+
+	createEvent(event: Event, start_date: string, end_date: string) {
+		return this.http.post(BASE_URL, event)
+			.map(response => response.json())
+			.catch(error => this.handleError(error));
+	}
+
+	deleteEvent(id: number | string) {
+		return this.http.delete(BASE_URL + id)
+			.map(response => response.json())
+			.catch(error => this.handleError(error));
+	}
+
+	updatedEvent(id: number | string, event: Event){
+		return this.http.put(BASE_URL + id,event)
             .map(response => response.json())
             .catch(error => this.handleError(error));
-    }
+	}
+
+	followEvent(id: number | string){
+		return this.http.put(BASE_URL + id, null)
+            .map(response => response.json())
+            .catch(error => this.handleError(error));
+	}
 
 
-    private handleError(error: any) {
+	private handleError(error: any) {
 		console.error(error);
 		return Observable.throw("Server error (" + error.status + "): " + error.text())
 	}
