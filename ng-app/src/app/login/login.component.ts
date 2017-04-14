695fec65071b1f673d760c22505eb6d20d2b469e
Frostqui <diego.lopezgarcia@hotmail.com>
2017-04-14 17:25:11 +0200

    Created component for login system. Added parameters to html template for login

--- ng-app/src/app/login/login.component.ts
+++ ng-app/src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
 import { LoginService } from './login.service';
+import { Http } from "@angular/http";
 
 @Component({
   templateUrl: './login.component.html',
@@ -9,4 +10,20 @@ import { LoginService } from './login.service';
 })
 export class LoginComponent{
 
+  nickname: string;
+  pass: string;
+
+
+ constructor(private http: Http, private service:LoginService) {}
+
+  
+
+ 
+
+  logIn(){
+    this.service.logIn(this.nickname ,this.pass).subscribe(
+      user => console.log(user)
+    );
+   
+  }
 }
