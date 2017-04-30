491541cc2e8827b9966c262b9aa327aa2f908b11
pablogarciabenayas <pablogarciabenayas@gmail.com>
2017-04-30 02:05:09 +0200

    Fix issues

--- ng-app/src/app/admin/admin-communities.component.ts
+++ ng-app/src/app/admin/admin-communities.component.ts
@@ -4,7 +4,7 @@ import { EventService } from "app/events/events.service";
 import { LoginService } from "app/login/login.service";
 import { Community } from "app/communities/community.model";
 import { CommunityService } from "app/communities/communities.service";
-import {PublicComponent} from '../public.component';
+import {PrivateComponent} from './admin.component';
 
 @Component({
   selector: 'admin-events',
@@ -22,10 +22,9 @@ export default class AdminCommunities implements OnInit {
 
      imgUrl = "https://localhost:8443/image/event-avatar/aleatorio/default-mainphoto";
 
-  constructor(private router:Router, private service: CommunityService, private sessionService: LoginService,private pubComponent: PublicComponent){}
+  constructor(private router:Router, private service: CommunityService, private sessionService: LoginService, private msgComponent: PrivateComponent){}
 
    ngOnInit() {
-    this.pubComponent.msgs.push({severity:'info', summary:'!!Eres administrador!!', detail:'Recuerda... Un gran poder conlleva una gran responsabilidad'});
     this.groupsPage = 0;
     this.moreGroupsButtonText = "Ver Más";
     this.service.getGroups(this.groupsPage).subscribe(
@@ -64,12 +63,12 @@ export default class AdminCommunities implements OnInit {
     this.service.deleteGroup(id).subscribe(
       response => {
         this.getCommunitys();
-        this.pubComponent.msgs.push({severity:'success', summary:'Comunidad eliminada', detail:'Comunidad eliminada satisfactoriamente'}); 
+        this.msgComponent.msgs.push({severity:'success', summary:'Comunidad eliminada', detail:'Comunidad eliminada satisfactoriamente'}); 
         
       },
       error => {
         console.log(error);
-        this.pubComponent.msgs.push({severity:'error', summary:'Error', detail:'Se ha producido un errror durante la eliminación de la comunidad'}); 
+        this.msgComponent.msgs.push({severity:'error', summary:'Error', detail:'Se ha producido un errror durante la eliminación de la comunidad'}); 
       }
     )
   }
